353e896e9733b02838fc14685fb80f75a32d396f
Bayo19 <bayo.300@gmail.com>
2021-10-06 13:49:57 +0100

    putting e and t functions in same file

--- extract_transform_bank_transaction_details.py
+++ extract_transform_bank_transaction_details.py
@@ -1,8 +1,16 @@
+import csv
 import etl_helper as e_helper
-import extract_transaction_details as extract
+            
+def extract(file, list_to_append_to):
+    with open(file, 'r') as file_name:
+        for row in csv.DictReader(file_name):
+            list_to_append_to.append(row)
+
 
 def transform(raw_dataset, list_to_read_to):
+    
     dataset_without_credit_transactions = e_helper.remove_credit_transactions(raw_dataset)
+    
     for row in dataset_without_credit_transactions:
         new_row = e_helper.remove_key_with_empty_value(row, 'Credit Amount')
         e_helper.delete_column(new_row, 'Account Number', 'Sort Code')
@@ -10,5 +18,9 @@ def transform(raw_dataset, list_to_read_to):
         list_to_read_to.append(new_row)    
 
 
+# Tables to make
+# transaction description (id, td_name)
+# debit - (td_id, amount spent, percentage of current balance)
 
+# objective of this task is to find out which places/people have been getting the most of my money and to also observe and do something about my spending habits
 
