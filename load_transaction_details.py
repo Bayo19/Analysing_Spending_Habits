59484b1502080251bf8b0eca395abe0f876cfca4
Bayo19 <bayo.300@gmail.com>
2021-10-07 21:33:39 +0100

    more sql statements :)

--- load_transaction_details.py
+++ load_transaction_details.py
@@ -24,17 +24,26 @@ def execute_command(connection, command):
 
 def create_transaction_description_table(con):
     command = '''CREATE TABLE if not exists transaction_descriptions(
-                td_id SERIAL(Primary Key),
+                td_id SERIAL,
                 td_name VARCHAR (255) NOT NULL)'''
                 
     execute_command(con, command)
 
 def create_purchases_table(con):
     command = '''CREATE TABLE if not exists purchases(
-                purchases_id(Primary Key),
-                td_id SERIAL,
-                amount_spent, season, percentage of current balance
-    )
-    '''
+                purchases_id,
+                td_id SERIAL NOT NULL,
+                amount_spent FLOAT(2) NOT NULL, 
+                season VARCHAR(255) NOT NULL, 
+                balance FLOAT(2))'''
+    execute_command(con, command)
+    
+def create_processed_files(con):
+    command = '''CREATE TABLE if not exists processed_files(
+                file_id SERIAL NOT NULL,
+                file_name VARCHAR(255) NOT NULL)'''
+    execute_command(con, command)
 
 create_transaction_description_table(connection)
+create_purchases_table(connection)
+create_processed_files(connection)
