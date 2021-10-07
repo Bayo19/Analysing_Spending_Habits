d6a172e18722264b549e70524efeb725ffe4da2c
Bayo19 <bayo.300@gmail.com>
2021-10-07 07:51:17 +0100

    adding notes and comments for things to be done in this task

--- extract_transform_bank_transaction_details.py
+++ extract_transform_bank_transaction_details.py
@@ -30,3 +30,11 @@ def transform(raw_dataset, list_to_read_to):
 # objective of this task is to find out which places/people have been getting the most of my money and to also observe and do something about my spending habits
 
 # add season to each row so that we can group by season later in sql
+
+# THINGS TO ANALYZE
+
+# .day in week where I spend the most money
+# .season where I spend the most money
+# .average amount of money spent per month over the years
+# .places where I spent the most money
+# .places where I shopped the most
