f722aebd4bd63dea8d564d5291127e2a6dfe44b0
Bayo19 <bayo.300@gmail.com>
2021-10-07 21:02:45 +0100

    writing sql scripts

--- load_transaction_details.py
+++ load_transaction_details.py
@@ -10,4 +10,31 @@ def create_connection():
     )
     return conn
 
-create_connection()
+connection = create_connection()
+
+def close_connection(cur):
+    cur.close()
+
+def execute_command(connection, command):
+    cur = connection.cursor()
+    cur.execute(command)
+    connection.commit()
+    close_connection(cur)
+
+
+def create_transaction_description_table(con):
+    command = '''CREATE TABLE if not exists transaction_descriptions(
+                td_id SERIAL(Primary Key),
+                td_name VARCHAR (255) NOT NULL)'''
+                
+    execute_command(con, command)
+
+def create_purchases_table(con):
+    command = '''CREATE TABLE if not exists purchases(
+                purchases_id(Primary Key),
+                td_id SERIAL,
+                amount_spent, season, percentage of current balance
+    )
+    '''
+
+create_transaction_description_table(connection)
