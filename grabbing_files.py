5df4bed5920b6a9a275e4960ccaf13c48ceed4d7
Bayo19 <bayo.300@gmail.com>
2021-10-09 15:40:40 +0100

    creating grab_file function and check_if_file_processed_ function

---
+++ grabbing_files.py
@@ -0,0 +1,10 @@
+import os
+def grab_file():
+    csv_files_in_dir = []
+    for root, dirs, files in os.walk('.'):
+        for name in files:
+            if 'csv_files' in root and '.csv' in name:
+                csv_files_in_dir.append(os.path.join(root,name))
+    return csv_files_in_dir
+                
+
